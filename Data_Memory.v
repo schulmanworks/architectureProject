acad89c42aea8bb08496044e8e2ec0dad704deeb
Ryan Schulman <schulmanworks@gmail.com>
2017-04-09 16:22:00 -0600

    fixed problems with initial blocks done

--- Data_Memory.v
+++ Data_Memory.v
@@ -3,12 +3,10 @@ module Data_Memory (
                     input [31:0] ALUOut, reg2data,
                     output reg [31:0] memout);
   reg [31:0] data_mem[199:0];
-  initial begin
-    data_mem[8] = 32'b11110000111100001111000011110000;
-    data_mem[10] = 32'b00000000000000000000000000000101;
-  end
   always @ (ALUOut or MemRead)
     begin
+    data_mem[8] = 32'b11110000111100001111000011110000;
+    data_mem[10] = 32'b00000000000000000000000000000101;
     if (MemRead == 1) memout = data_mem[ALUOut];
   end
   always @(posedge clk)
--- test.v
+++ test.v
@@ -7,8 +7,4 @@ initial begin
 forever #10 clk = ~clk;
 end
 processor H0 (reset, clk);
-reg[31:0] register[31:0];
-initial begin register[0] = 0; end // $zero
-assign regldata = register[reg1ad]; // read
-assign reg2data = register[reg2ad]; // write
 endmodule
